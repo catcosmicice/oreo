8a2650be21384c2aba032489fa159fe3b3a1327e
cosmicice <catcosmicice@protonmail.ch>
2021-04-12 05:42:37 +0000

    .

--- src/commands/Moderation/ban.ts
+++ src/commands/Moderation/ban.ts
@@ -5,8 +5,7 @@ export default class Ban extends Command {
     constructor() {
         super('ban', {
             description: (language: Language) => language.get('BAN_COMMAND_DESCRIPTION'),
-            aliases: ['bean', 'yeet', '410', 'gtfo', 'bonk', 'begone', 'banish'],
-            clientPermissions
+            aliases: ['bean', 'yeet', '410', 'gtfo', 'bonk', 'begone', 'banish']
         })
     }
 }
